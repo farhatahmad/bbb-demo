98cb4a6b2a080fc37eb19e6b47675feb82e2637c
farhatahmad <ahmad.af.farhat@gmail.com>
2019-01-08 10:20:25 -0500

    Basic backend setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,48 @@
+require 'bigbluebutton_api'
+
+class PagesController < ApplicationController
+  def home
+
+  end
+
+  def create_and_join_meeting
+    prepare()
+
+
+    create()
+
+
+    join()
+  end
+
+  def prepare
+    url = "http://10.8.186.201/bigbluebutton/api"
+    secret = "eae6b20cd43c742e3f89b49eebfeeaf1"
+    version = 0.81
+
+    @api = BigBlueButton::BigBlueButtonApi.new(url,secret,version.to_s, true)
+  end
+
+  def create
+    @meeting_name = "Test Meeting 1"
+    @meeting_id = "test-meeting-1"
+    @options = {
+       :moderatorPW => "mp",
+       :attendeePW => "ap",
+     }
+
+    unless @api.is_meeting_running?(@meeting_id)
+      response = @api.create_meeting(@meeting_name, @meeting_id, @options)
+      puts "The meeting has been created with the response:"
+      puts response.inspect
+    else
+      puts "Meeting already running"
+    end
+  end
+
+  def join
+    url = @api.join_meeting_url(@meeting_id,"Ahmad", @options[:moderatorPW])
+    puts "1) Moderator URL = #{url}"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "/" => "pages#home"
+  get "create/" => "pages#create_and_join_meeting"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

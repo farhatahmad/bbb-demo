921c072623bc54844af0c2950c0bfc36d3fe49bc
farhatahmad <ahmad.af.farhat@gmail.com>
2019-01-09 16:44:54 -0500

    Added a list of current available rooms + styling changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,21 @@
 require 'bigbluebutton_api'
 
 class PagesController < ApplicationController
-  def home; end
+  def home
+    intialize_api_connection
 
-  def create_and_join_meeting
-    prepare
-
-    create(request.params[:room_id])
-
-    join(request.params[:name])
+    @current_rooms_created = @api.get_meetings
   end
 
-  def prepare
+  def create_and_join_meeting
+    intialize_api_connection
+
+    create_room(request.params[:room_id])
+
+    join_room(request.params[:name])
+  end
+
+  def intialize_api_connection
     url = 'http://10.8.186.201/bigbluebutton/api'
     secret = 'eae6b20cd43c742e3f89b49eebfeeaf1'
     version = 0.81
@@ -19,7 +23,7 @@ class PagesController < ApplicationController
     @api = BigBlueButton::BigBlueButtonApi.new(url, secret, version.to_s, true)
   end
 
-  def create(room_id = "default-room-id")
+  def create_room(room_id = "default-room-id")
     @meeting_name = room_id.gsub('-', ' ').titleize
     @meeting_id = room_id
     @options = {
@@ -35,7 +39,7 @@ class PagesController < ApplicationController
     end
   end
 
-  def join(username)
+  def join_room(username)
     url = @api.join_meeting_url(@meeting_id, username, @options[:moderatorPW])
     redirect_to url.to_s
   end
